21a2070fa48cfe7bd7e4b7597d5fe440ffe5bd56
jysonn97 <juyeong.son97@gmail.com>
2025-03-17 11:09:45 -0400

    Update app.js

--- pages/app.js
+++ pages/app.js
@@ -24,7 +24,7 @@ export default function AppPage() {
     const updatedAnswers = [...answers, { question, answer }];
 
     try {
-      const response = await fetch("/api/generateQuestion", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/generateQuestion`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ previousAnswers: updatedAnswers }),

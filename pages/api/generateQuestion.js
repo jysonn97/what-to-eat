14260b509e05e5b098b3ed6af493947c8d54c33e
jysonn97 <juyeong.son97@gmail.com>
2025-03-21 14:43:48 -0400

    Update generateQuestion.js

--- pages/api/generateQuestion.js
+++ pages/api/generateQuestion.js
@@ -13,7 +13,8 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+
       },
       body: JSON.stringify({
         model: "gpt-4-turbo",

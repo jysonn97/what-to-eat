811779847b8c471a70ffcc164909dd884677208a
jysonn97 <juyeong.son97@gmail.com>
2025-04-29 13:20:06 -0400

    Create QuestionCard.js

---
+++ components/QuestionCard.js
@@ -0,0 +1,8 @@
+// components/QuestionCard.js
+export default function QuestionCard({ question }) {
+  return (
+    <h1 className="text-2xl font-semibold text-gray-900 text-center">
+      {question}
+    </h1>
+  );
+}

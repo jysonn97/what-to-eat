fcd3696d20cd962331cfd921c0537eb879f99766
jysonn97 <juyeong.son97@gmail.com>
2025-04-30 14:25:44 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+// ✅ Load Inter with extra-light weight (200) included
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+});
 
 export default function App({ Component, pageProps }) {
   return (

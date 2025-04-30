e1cdace883db4c3cacf53548e723f8ca8de64e04
jysonn97 <juyeong.son97@gmail.com>
2025-04-30 11:59:51 -0400

    Update QuestionCard.js

--- components/QuestionCard.js
+++ components/QuestionCard.js
@@ -1,6 +1,6 @@
 export default function QuestionCard({ question }) {
   return (
-    <h1 className="text-center text-xl font-extralight text-white">
+    <h1 className="text-center text-2xl font-extralight text-white">
       {question}
     </h1>
   );

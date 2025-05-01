72ddb040028da94f9b9764dc1d5655d358c77dfb
jysonn97 <juyeong.son97@gmail.com>
2025-05-01 13:18:49 -0400

    Update app.js

--- pages/app.js
+++ pages/app.js
@@ -1,3 +1,4 @@
+import CuisineGrid from "@/components/CuisineGrid";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import QuestionCard from "@/components/QuestionCard";

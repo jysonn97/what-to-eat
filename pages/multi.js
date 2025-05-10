a8875887719e11d548cf7f5f5a4c975131fb77fe
jysonn97 <juyeong.son97@gmail.com>
2025-05-10 16:45:43 -0400

    Update multi.js

--- pages/multi.js
+++ pages/multi.js
@@ -159,10 +159,10 @@ className={`px-3 py-1 text-xs rounded-md border transition min-w-[120px] text-ce
 </div>
 
 {/* Buttons */}
-<div className="pt-6 flex flex-col items-center gap-[4px]">
+<div className="pt-6 flex flex-col items-center gap-[7px]">
   <button
     onClick={clearAll}
-    className="text-xs underline text-white font-light hover:opacity-90 mt-4"
+    className="text-xs underline text-white font-light hover:opacity-90 mt-7"
   >
     Clear All
   </button>
